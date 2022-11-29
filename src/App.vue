ef57cb504cf878cfc9add501dd30883e13f12faa
cooljin <1242703278@qq.com>
2022-11-29 16:35:17 +0800

    引入vant组件库 以及图标组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <router-view/>
+    <i class="toutiao toutiao-lishi"></i>
     <h1>芝士黑马头条</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
