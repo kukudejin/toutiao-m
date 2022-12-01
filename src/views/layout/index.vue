45640bd1daccfa964ae2c5e77e003b1fc2c6f7b3
李锦 <1242703278@qq.com>
2022-12-01 11:51:27 +0800

    完成了大部分TabBar的内容首页高未解决

---
+++ src/views/layout/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="layout-container">
+    <!-- 子路由出口 -->
+    <router-view />
+    <!-- 子路由出口 -->
+    <!-- 标签栏导航 -->
+    <!-- route 开启路由模式 -->
+<van-tabbar class="layout-tabbar" v-model="active" route>
+  <van-tabbar-item to="/">
+    <template #icon>
+      <div class="toutiao toutiao-shouye"></div>
+    </template>
+    <span class="text">首页</span>
+  </van-tabbar-item>
+
+  <van-tabbar-item to="/qa">
+    <template #icon>
+      <div class="toutiao toutiao-wenda"></div>
+    </template>
+    <span class="text">问答</span>
+  </van-tabbar-item>
+
+  <van-tabbar-item to="/video">
+    <template #icon>
+      <div class="toutiao toutiao-shipin"></div>
+    </template>
+    <span class="text">视频</span>
+  </van-tabbar-item>
+
+  <van-tabbar-item to="/my">
+    <template #icon>
+      <div class="toutiao toutiao-wode"></div>
+    </template>
+    <span class="text">我的</span>
+  </van-tabbar-item>
+</van-tabbar>
+    <!-- /标签栏导航 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      active: 0
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+.layout-container{
+  .layout-container{
+    .layout-tabbar{
+      i.toutiao{
+        font-size: 40px;
+      }
+      span.text{
+        font-size: 20px;
+      }
+    }
+  }
+}
+</style>
